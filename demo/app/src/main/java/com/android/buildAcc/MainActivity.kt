a63dff31f5e417a985abbbff5746085b6ed90cad
peipei.zhu <peipei.zhu@ximalaya.com>
2022-11-08 17:32:46 +0800

    [feat] 添加Library1库

--- demo/app/src/main/java/com/android/buildAcc/MainActivity.kt
+++ demo/app/src/main/java/com/android/buildAcc/MainActivity.kt
@@ -2,10 +2,13 @@ package com.android.buildAcc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.library1.Library1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Library1.print()
     }
 }
---
+++ demo/library1/src/main/java/com/android/library1/Library1.java
@@ -0,0 +1,13 @@
+package com.android.library1;
+
+import android.util.Log;
+
+/**
+ * @author ZhuPeipei
+ * @date 2022/11/8 17:23
+ */
+public class Library1 {
+    public static void print() {
+        Log.i("Library1", "print library1");
+    }
+}
