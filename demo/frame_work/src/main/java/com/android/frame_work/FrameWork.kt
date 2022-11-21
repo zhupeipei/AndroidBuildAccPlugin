827e85fa81e6e811d26a30b28d592d78dcbf2b41
zhupeipei <zhupeipei91@163.com>
2022-11-21 20:11:52 +0800

    [feat] 添加frame_work和host库

---
+++ demo/frame_work/src/main/java/com/android/frame_work/FrameWork.kt
@@ -0,0 +1,13 @@
+package com.android.frame_work
+
+import android.util.Log
+
+/**
+ * @author ZhuPeipei
+ * @date 2022/11/9 22:44
+ */
+object FrameWork {
+    fun test() {
+        Log.i("FrameWork", "test FrameWork")
+    }
+}
---
+++ demo/host/src/main/java/com/android/host/Host.kt
@@ -0,0 +1,13 @@
+package com.android.host
+
+import android.util.Log
+
+/**
+ * @author ZhuPeipei
+ * @date 2022/11/9 22:44
+ */
+object Host {
+    fun test() {
+        Log.i("Host", "test host")
+    }
+}
