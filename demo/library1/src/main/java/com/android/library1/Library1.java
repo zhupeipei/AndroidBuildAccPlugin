0b2d5b0711cf5f2ecf4ef9c7554e05b81b024253
zhupeipei <zhupeipei91@163.com>
2022-11-21 21:39:31 +0800

    [feat] 解决编译问题

--- demo/library1/src/main/java/com/android/library1/Library1.java
+++ demo/library1/src/main/java/com/android/library1/Library1.java
@@ -3,7 +3,6 @@ package com.android.library1;
 import android.util.Log;
 
 import com.android.library11.Library11Test;
-import com.hihonor.push.sdk.utils.HonorIdUtils;
 
 /**
  * @author ZhuPeipei
@@ -13,6 +12,6 @@ public class Library1 {
     public static void print() {
         Log.i("Library1", "print library1");
         Library11Test.test();
-        new HonorIdUtils();
+//        new HonorIdUtils();
     }
 }
